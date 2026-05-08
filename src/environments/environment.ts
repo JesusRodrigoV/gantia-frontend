98a8779f676917d894de88125e75b6a19f5c9dba
JesusRodrigoV <jaicelvelasco105@gmail.com>
2026-05-08 14:06:01 -0400

    cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const env = {
   production: false,
   // apiUrl: 'https://pelvis-handrail-rigid.ngrok-free.dev',
   apiUrl: 'http://localhost:8000',
-  wsUrl: 'wss://pelvis-handrail-rigid.ngrok-free.dev',
+  wsUrl: 'wss://localhost:8000',
+  // wsUrl: 'wss://pelvis-handrail-rigid.ngrok-free.dev',
 };
